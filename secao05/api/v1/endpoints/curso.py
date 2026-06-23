5b48469ed9597c3708a7a87da649ec7bcbb78f3b
Leonardo Souza <leonardoleoleite21@gmail.com>
2026-06-23 16:47:43 -0300

    Att curso secao 6

---
+++ secao05/api/v1/endpoints/curso.py
@@ -0,0 +1,32 @@
+from typing import List
+
+from fastapi import APIRouter
+from fastapi import status
+from fastapi import Depends
+from fastapi import HTTPException
+from fastapi import Response
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
+from models.curso_model import CursoModel
+from core.deps import get_session
+
+# Bypass Warning SQLModel select
+from sqlmodel.sql.expression import Select, SelectOfScalar
+
+SelectOfScalar.inherit_cache = True  # type: ignore
+Select.inherit_cache = True  # type: ignore
+# Fim Bypass
+
+router = APIRouter()
+
+# POST CURSO
+@router.post('/', status_code=status.HTTP_201_CREATED, response_model=CursoModel)
+async def post_curso(curso: CursoModel, db: AsyncSession = Depends(get_session)):
+    novo_curso = CursoModel(titulo=curso.titulo, aulas=curso.aulas, horas=curso.horas)
+
+    db.add(novo_curso)
+    await db.commit()
+
+    return novo_curso
---
+++ secao06/core/configs.py
@@ -0,0 +1,27 @@
+from typing import Any, ClassVar
+
+from pydantic_settings import BaseSettings
+from sqlalchemy.ext.declarative import declarative_base
+
+class Settings(BaseSettings):
+    """
+    Configurações gerais usadas na aplicação
+    """
+    API_V1_STR: str = '/api/v1'
+    DB_URL: str = "postgresql+asyncpg://postgres:1234@localhost:5432/faculdade"
+    DBBaseModel: ClassVar[Any] = declarative_base()
+
+    JWT_SECRET: str = '7eYCvy1VQX6pZthMLEM8-YQmttqfAfBNg-wJgs-j6R8'
+    """
+    import secrets
+
+    token: str = secrets.token_urlsafe(32)
+    """
+    ALGORITHM: str = 'HS256'
+    # 60 minutos * 24 horas * 7 dias => 1 semana
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7
+
+    class Config:
+        case_sensitive = True
+
+settings: Settings = Settings()
